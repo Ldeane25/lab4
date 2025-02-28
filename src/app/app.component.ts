3af448e58e1488c45bb284c4267f4efc41dc4ac5
unknown <lukedeane775@gmail.com>
2025-02-28 13:27:35 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import{ListComponent}from './list/list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'data-binding-app';
+  count: number = 0
+  blHide: boolean = false;
+  onButtonClick() {
+    this.count++
+
+  }
+  onStarDBLclicked() {
+   if(this.blHide){
+    this.blHide = false
+   }
+    else{
+      this.blHide = true
+    }
+  }
 }
